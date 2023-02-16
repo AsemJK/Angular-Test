a78c2a6b882a783a38c3314d9240c5888bdb2f09
AsemJK <asemalsaiyadi@gmail.com>
2023-02-16 07:04:40 +0300

    2

--- src/app/test-element/test-element.component.ts
+++ src/app/test-element/test-element.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-test-element',
   templateUrl: './test-element.component.html',
   styleUrls: ['./test-element.component.css']
 })
-export class TestElementComponent {
+export class TestElementComponent implements OnInit {
+
+  @ViewChild('serverNameInput') HtmlPartAsViewChild: ElementRef | undefined;
+
+  ngOnInit(): void {
+  }
 
 
-  sendLocalReference(passedData:any){
+  sendLocalReference(passedData:any)
+  {
     console.log(passedData.value);
+
+    console.log(this.HtmlPartAsViewChild?.nativeElement.value);
+
   }
 }
